fda5aab2bbd3d54700178164585ee8597729d35c
Igor Enrick <igorenrickc@gmail.com>
2019-11-25 01:15:11 -0300

    Adiciona QR Code

--- src/screens/app/Use.js
+++ src/screens/app/Use.js
@@ -16,16 +16,35 @@ import {
   TouchableHighlight
 } from 'react-native';
 
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+
+import QRCodeScanner from 'react-native-qrcode-scanner';
+
 const screenWidth = Math.round(Dimensions.get('window').width)
+const screenHeight = Math.round(Dimensions.get('window').height)
 
 const UseResult = ({ navigation }) => {
+  onSuccess = (e) => {
+    navigation.navigate('UseResult')
+  }
   return (
     <>
-      <StatusBar backgroundColor="#0A84FF"  barStyle="light-content" />
+      <StatusBar backgroundColor="#000"  barStyle="light-content" />
       <View style={styles.scrollView}>
-        <TouchableHighlight style={styles.button} underlayColor={'#0972DC'} onPress={() => navigation.navigate('UseResult')}>
-            <Text style={styles.text}>{"USE RESULT"}</Text>
-        </TouchableHighlight>
+        <QRCodeScanner
+            onRead={() => onSuccess()}
+            showMarker={true}
+            //cameraStyle={{height: screenHeight}}
+            topContent={
+                <View style={styles.headerView}>
+                    <Icon name="account-circle-outline" size={32} color={'#0A84FF'} />
+                    <Text style={styles.nome}>Igor Enrick</Text>
+                </View>
+            }
+            bottomContent={
+                <Text style={styles.desc}>{'Aproxime do Código QR no\nrestaurante para utilizar o cartão.'}</Text>
+            }
+        />
       </View>
     </>
   );
@@ -33,10 +52,28 @@ const UseResult = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   scrollView: {
-    backgroundColor: '#0A84FF',
+    backgroundColor: '#000',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  headerView: {
+    flexDirection: 'row',
+    marginTop: -40,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  nome: {
+    fontFamily: 'Roboto-Bold',
+    color: '#0A84FF',
+    fontSize: 18,
+    marginLeft: 5
+  },
+  desc: {
+      marginHorizontal: 40,
+      color: '#fff',
+      fontFamily: 'Roboto-Italic',
+      textAlign: 'center'
   }
 });
 
